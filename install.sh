7154fec88d23f82ea60c2d11ed27d7dda3d546e9
Javier Cañadillas <javiercm@google.com>
2021-11-10 19:06:39 +0100

    Fixed typos

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 REPO_NAME="cloudshell-dotfiles"
 
 # Rename repo directory
-mv "${HOME/REPO_NAME}" "${HOME/.dotfiles}"
+mv "${HOME}/${REPO_NAME}" "${HOME}/.dotfiles"
 
 # Link relevant files to $HOME
 ln -s "${HOME}"/.bash_profile "${HOME}/${REPO_NAME}/bash_profile"

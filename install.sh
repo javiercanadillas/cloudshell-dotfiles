3d95f13b86bd5040b75bd3ed8826fa9bddfecbf5
Javier Cañadillas <javiercm@google.com>
2021-11-10 19:18:35 +0100

    Fixed dotfiles dir renaming

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 REPO_NAME="cloudshell-dotfiles"
+DOTFILES_LOC="${HOME}/.dotfiles"
 
 # Rename repo directory
-mv "${HOME}/${REPO_NAME}" "${HOME}/.dotfiles"
+mv "${HOME}/${REPO_NAME}" "${DOTFILES_LOC}"
 
 # Link relevant files to $HOME
-ln -s "${HOME}/${REPO_NAME}/bash_profile" "${HOME}"/.bash_profile
+ln -s "${DOTFILES_LOC}"/bash_profile" "${HOME}"/.bash_profile

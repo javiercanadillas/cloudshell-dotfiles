b5fa7620ce27bfb862a238e5ea5197a8180f7bbd
Javier Cañadillas <72024845+javiercanadillas@users.noreply.github.com>
2022-03-08 15:46:14 +0100

    Update install.sh
    Wrapped some code into functions

--- install.sh
+++ install.sh
@@ -12,21 +12,21 @@ git_config(){
   git config --global user.email "GIT_PASSWORD"
 }
 
+## Install Bat, an improved and colorful cat
+install_bat(){
+  command -v wget >/dev/null  2>&1 && wget "${BAT_REPO_URL}"
+  sudo dpkg -i *.deb
+  mkdir -p "${LOCAL_BIN_DIR}"
+  command -v batcat >/dev/null  2>&1 && ln -s /usr/bin/batcat "${LOCAL_BIN_DIR}/bat" 
+  rm *.deb
+}
 
 # Rename repo directory
 mv "${HOME}/${REPO_NAME}" "${DOTFILES_LOC}"
-
-# Install useful tools
-## Install Bat, an improved and colorful cat
-command -v wget >/dev/null  2>&1 && wget "${BAT_REPO_URL}"
-sudo dpkg -i *.deb
-mkdir -p "${LOCAL_BIN_DIR}"
-command -v batcat >/dev/null  2>&1 && ln -s /usr/bin/batcat "${LOCAL_BIN_DIR}/bat" 
-rm *.deb
-
+git_config() # Configure git
+install_bat() # Install command line bat
 # Link relevant files to $HOME
 ln -s "${DOTFILES_LOC}/bash_profile" "${HOME}/.bash_profile"
-
 # Remind the user to source .bash_profile
 pushd $HOME
 source .bash_profile
